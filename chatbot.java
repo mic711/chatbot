7a5fcde16a66c3c28a757dbb02379112dde6fde0
mic711 <32948228+mic711@users.noreply.github.com>
2018-12-12 03:16:13 -0800

    Update chatbot.java

--- chatbot.java
+++ chatbot.java
@@ -42,56 +42,59 @@ public class Magpie4
         {
             response = "Why so negative?";
         }
-        else if (findKeyword(statement, "music") >= 0
-                || findKeyword(statement, "harmony") >= 0
-                || findKeyword(statement, "chord") >= 0
-                || findKeyword(statement, "note") >= 0
-                || findKeyword(statement, "semitone") >= 0
-                || findKeyword(statement, "theory") >= 0
-                || findKeyword(statement, "augment") >= 0
-                || findKeyword(statement, "major") >= 0
-                || findKeyword(statement, "minor") >= 0
-                || findKeyword(statement, "octave") >= 0
-                || findKeyword(statement, "step") >= 0
-                || findKeyword(statement, "pitch") >= 0)
+        else if (findKeyword(statement, "NBA Basketball teams") >= 0
+                || findKeyword(statement, "Warriors") >= 0
+                || findKeyword(statement, "Raptors") >= 0
+                || findKeyword(statement, "Rockets") >= 0
+                || findKeyword(statement, "Lakers") >= 0
+                || findKeyword(statement, "Spurs") >= 0
+                || findKeyword(statement, "Bucks") >= 0
+                || findKeyword(statement, "Knicks") >= 0
+                || findKeyword(statement, "Nets") >= 0
+                || findKeyword(statement, "Magic") >= 0
+                || findKeyword(statement, "Clippers") >= 0
+                || findKeyword(statement, "Thunder") >= 0)
         {
-            response = "Tell me more about music theory.";
+            response = "Tell me more about your favorite basketball team.";
         }
         
         
-                else if (findKeyword(statement, "piano") >= 0
-                || findKeyword(statement, "instrument") >= 0
-                || findKeyword(statement, "guitar") >= 0
-                || findKeyword(statement, "violin") >= 0
-                || findKeyword(statement, "band") >= 0
-                || findKeyword(statement, "orchestra") >= 0
-                || findKeyword(statement, "competition") >= 0
-                || findKeyword(statement, "practice") >= 0
-                || findKeyword(statement, "play") >= 0
-                || findKeyword(statement, "drum") >= 0
-                || findKeyword(statement, "beat") >= 0
-                || findKeyword(statement, "conductor") >= 0)
+                else if (findKeyword(statement, "Stephen Curry") >= 0
+                || findKeyword(statement, "LeBron James") >= 0
+                || findKeyword(statement, "Kevin Durant") >= 0
+                || findKeyword(statement, "Russel Westbrook") >= 0
+                || findKeyword(statement, "Paul George") >= 0
+                || findKeyword(statement, "Damian Lillard") >= 0
+                || findKeyword(statement, "Giannis Antetokounmpo") >= 0
+                || findKeyword(statement, "James Harden") >= 0
+                || findKeyword(statement, "Demar Derozen") >= 0
+                || findKeyword(statement, "Blake Griffin") >= 0
+                || findKeyword(statement, "Klay Thompson") >= 0
+                || findKeyword(statement, "Draymond Green") >= 0)
         {
-            response = "Tell me more about the instrument you practice.";
+            response = "Tell me more about your favorite player.";
         }
 
-        
-        
-                else if (findKeyword(statement, "difficult") >= 0
-                || findKeyword(statement, "hard") >= 0
-                || findKeyword(statement, "easy") >= 0
-                || findKeyword(statement, "skill") >= 0
-                || findKeyword(statement, "level") >= 0
-                || findKeyword(statement, "metronome") >= 0
-                || findKeyword(statement, "teacher") >= 0
-                || findKeyword(statement, "rehersal") >= 0
-                || findKeyword(statement, "practice") >= 0
-                || findKeyword(statement, "seat") >= 0
-                || findKeyword(statement, "time") >= 0
-                || findKeyword(statement, "hour") >= 0)
+                else if (findKeyword(statement, "CURRY 5") >= 0
+                || findKeyword(statement, "LEBRON SOLDIER XII") >= 0
+                || findKeyword(statement, "KYRIE 4") >= 0
+                || findKeyword(statement, "PG 2.5") >= 0
+                || findKeyword(statement, "NIKE HYPERDUNK X") >= 0
+                || findKeyword(statement, "DAME 4") >= 0
+                || findKeyword(statement, "HARDEN VOL.2") >= 0
+                || findKeyword(statement, "KlAY ANTA KT3") >= 0
+                || findKeyword(statement, "ADDIAS CRAZYLIGHT BOAST") >= 0
+                || findKeyword(statement, "AIR JORDAN 11") >= 0
+                || findKeyword(statement, "NIKE KD 6") >= 0
+                || findKeyword(statement, "REEBOK SHAQNOSIS") >= 0)
         {
-            response = "Tell me more about your practice routine.";
+            response = "Tell me more about your favorite basketball shoe.";
         }
+	    else if(findKeyword(statement, "My favorite nickname for a player is")>= 0)
+	{
+            response = "That nickname for that player is creative and cool.";
+        }
+
         // Responses which require transformations
         else if (findKeyword(statement, "I want to", 0) >= 0)
         {
@@ -101,56 +104,68 @@ public class Magpie4
         // Responses which require transformations
         else if (findKeyword(statement, "I want to", 0) >= 0)
         {
-            response = transformIPlayStatement(statement);
+            response = transformIWantToStatement(statement);
         }
 
         else
-        {
-            // Look for a two word (you <something> me)
-            // pattern
-            int psn = findKeyword(statement, "you", 0);
-
-            if (psn >= 0
-                    && findKeyword(statement, "me", psn) >= 0)
-            {
-                response = transformYouMeStatement(statement);
-            }
-            else
-            {
-                response = getRandomResponse();
-            }
-        }
-        return response;
+     {
+       if (hasXThenY(statement, "you", "me")) {
+				response = transformYouMeStatement(statement);
+			}
+			else if (hasXThenY(statement, "I", "you")) {
+				response = transformIYouStatement(statement);
+			}
+			else
+			{
+				response = getRandomResponse();
+			}
     }
-    
+
+
+    private boolean hasXThenY(String statement, String first, String second)
+	{
+		// Look for a two word pattern
+		int psn = findKeyword(statement, first, 0);
+		return psn >= 0 && findKeyword(statement, second, psn) >= 0;
+	}
     /**
      * Take a statement with "I want to <something>." and transform it into 
      * "What would it mean to <something>?"
      * @param statement the user statement, assumed to contain "I want to"
      * @return the transformed statement
      */
-    private String transformIWantToStatement(String statement)
-    {
-        //  Remove the final period, if there is one
-        statement = statement.trim();
-        String lastChar = statement.substring(statement
-                .length() - 1);
-        if (lastChar.equals("."))
-        {
-            statement = statement.substring(0, statement
-                    .length() - 1);
-        }
-        int psn = findKeyword (statement, "I want to", 0);
-        String restOfStatement = statement.substring(psn + 9).trim();
-        return "What would it mean to " + restOfStatement + "?";
+	private String transformIWantToStatement(String statement)
+	{
+		return transformSingle(statement, "I want to ", "What would it mean to ", "?");
+	}
+    private String transformSingle(String statement, String keyword, String before, String after)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, keyword, 0);
+		String restOfStatement = statement.substring(psn + keyword.length()).trim();
+		return "What would it mean to" + restOfStatement + after;	
+	}
     }
 
-    /**
-     * Take a statement with "I play <something>." and transform it into 
-     * "You play <something>?"
-     * @param statement the user statement, assumed to contain "I want to"
-     * @return the transformed statement
-     */
+   /**
+	 * Take a statement with "The best team is <something>" and transform it into 
+	 * "What makes you think that the best team is <something>?"
+	 * @param statement the user statement, assumed to contain "you" followed by "me"
+	 * @return the transformed statement
+	 */
+	private String transformYouMeStatement(String statement)
+	{
+		return transformInner(statement, "The best team is", "What makes you think that the best team is " "?");
+	}
+
     private String transformIPlayStatement(String statement)
     {
         //  Remove the final period, if there is one
@@ -164,16 +179,20 @@ public class Magpie4
         }
         int psn = findKeyword (statement, "I want to", 0);
         String restOfStatement = statement.substring(psn + 9).trim();
-        return "You play  " + restOfStatement + "?";
+        return "What makes you think that the best team is " + restOfStatement + "?";
     }
     
-    /**
-     * Take a statement with "you <something> me" and transform it into 
-     * "What makes you think that I <something> you?"
-     * @param statement the user statement, assumed to contain "you" followed by "me"
+/**
+     * Take a statement with "I play for <something>." and transform it into 
+     * "What basketball team do you play for <something>?"
+     * @param statement the user statement, assumed to contain "I want to"
      * @return the transformed statement
      */
-    private String transformYouMeStatement(String statement)
+	private String transformYouMeStatement(String statement)
+	{
+		return transformInner(statement, "I play for, "What basketball team do you play for " "?");
+	}
+    private String transformIPlayStatement(String statement)
     {
         //  Remove the final period, if there is one
         statement = statement.trim();
@@ -184,12 +203,9 @@ public class Magpie4
             statement = statement.substring(0, statement
                     .length() - 1);
         }
-        
-        int psnOfYou = findKeyword (statement, "you", 0);
-        int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
-        
-        String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
-        return "What makes you think that I " + restOfStatement + " you?";
+        int psn = findKeyword (statement, "I want to", 0);
+        String restOfStatement = statement.substring(psn + 9).trim();
+        return "What basketball team do you play for  " + restOfStatement + "?";
     }
     
     
@@ -197,13 +213,17 @@ public class Magpie4
     
     
     /**
-     * Search for one word in phrase.  The search is not case sensitive.
+     * Search for one word in phrase.  
      * This method will check that the given goal is not a substring of a longer string
      * (so, for example, "I know" does not contain "no").  
      * @param statement the string to search
      * @param goal the string to search for
      * @param startPos the character of the string to begin the search at
      * @return the index of the first occurrence of goal in statement or -1 if it's not found
+     * In sum, The algorithm first looks at the string response in the parameters. Then it searches for one word in the phrase
+      or response. It also takes an initial position in the beginning. This method will check that the given goal 
+      is not a substring of a longer string. Will first convert the phrase string to lowercase, then convert the goal string to lowercase, 
+      then lastly, find the first index of the goal lowercase string in the phrase lowercase string, after the start position.
      */
     private int findKeyword(String statement, String goal, int startPos)
     {
@@ -261,7 +281,7 @@ public class Magpie4
      */
     private String getRandomResponse()
     {
-        final int NUMBER_OF_RESPONSES = 4;
+        final int NUMBER_OF_RESPONSES = 10;
         double r = Math.random();
         int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
         String response = "";
